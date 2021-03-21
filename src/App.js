7ffdc62627b7e916793dbc09d504bdbccacf7e7e
Jihad Banna <super_banna@hotmail.com>
2021-03-21 16:43:30 +0200

    Basic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+//Pages
+import Home from "./pages/Home/Home.js";
 
 function App() {
   return (
-    <div className="App">
-      <nav className="Nav">
-        <navBar />
-      </nav>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+
+        <Redirect to="/404" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import "../Home/Home.css";
+
+function homePage() {
+  return (
+    <div className="App">
+      <nav className="navBar">
+        <h1>الموجه الاكاديمي</h1>
+      </nav>
+    </div>
+  );
+}
+
+export default homePage;
