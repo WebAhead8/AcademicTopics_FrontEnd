28beb803905793cbe6dfaa9ec6600cc44d27dcbf
Jihad Banna <super_banna@hotmail.com>
2021-03-22 10:00:06 +0200

    icon

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,12 @@
 import "../Home/Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function homePage() {
   return (
     <div className="App">
       <nav className="navBar">
+        <FontAwesomeIcon icon={faBars} />
         <h1>الموجه الاكاديمي</h1>
       </nav>
     </div>
