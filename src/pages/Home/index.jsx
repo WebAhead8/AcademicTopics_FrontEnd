a2e47eb5a48b2ddc21cadd6a7393900d029a42f6
Jihad Banna <super_banna@hotmail.com>
2021-03-24 16:32:52 +0200

    up

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,23 @@
 import "./style.css";
 import Nav from "../../components/navBar/index";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   return (
     <div className="App">
       <Nav />
+      <div className="divSearch">
+        <label htmlFor="mainSearch" className="mainSearch">
+          <FontAwesomeIcon icon={faSearch} />
+          <input
+            type="text"
+            name="mainSearch"
+            id="mainSearch"
+            placeholder="جامعات/مواضيع/مرشدين"
+          />
+        </label>
+      </div>
     </div>
   );
 }
