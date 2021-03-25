4174e9663546452bde8c2f685858f8b4afc3a978
AlaaMath <alaa.lathqani@gmail.com>
2021-03-25 14:33:53 +0200

    uni

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 //Pages
-import Home from "./pages/Home";
-import Fit from "./pages/fit";
-import LogIn from "./pages/login";
-import SignUp from "./pages/signup";
+import Home from './pages/Home';
+import Fit from './pages/fit';
+import LogIn from './pages/login';
+import SignUp from './pages/signup';
+import AcademicInstitutes from './pages/academic_institutes';
 // import Subjects from "./pages/subjects";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/fit" component={Fit} />
-        <Route exact path="/login" component={LogIn} />
-        <Route exact path="/signup" component={SignUp} />
-        {/* <Route exact path="/subject" component={subjects} /> */}
-        <Redirect to="/404" />
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/fit" component={Fit} />
+				<Route exact path="/login" component={LogIn} />
+				<Route exact path="/signup" component={SignUp} />
+				<Route exact path="/academic_institutes" component={AcademicInstitutes} />
+				{/* <Route exact path="/subject" component={subjects} /> */}
+				<Redirect to="/404" />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
