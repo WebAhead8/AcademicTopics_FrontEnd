0a664021c3939955eae1f08d31141ef8376c3550
AlaaMath <alaa.lathqani@gmail.com>
2021-03-28 17:28:37 +0300

    subjects

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Fit from './pages/fit';
 import LogIn from './pages/login';
 import SignUp from './pages/signup';
 import AcademicInstitutes from './pages/academic_institutes';
-// import Subjects from "./pages/subjects";
+import Subjects from './pages/subjects';
 
 function App() {
 	return (
@@ -18,7 +18,7 @@ function App() {
 				<Route exact path="/login" component={LogIn} />
 				<Route exact path="/signup" component={SignUp} />
 				<Route exact path="/academic_institutes" component={AcademicInstitutes} />
-				{/* <Route exact path="/subject" component={subjects} /> */}
+				<Route exact path="/subjects" component={Subjects} />
 				<Redirect to="/404" />
 			</Switch>
 		</Router>
--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,51 +1,52 @@
-import "./navBar.css";
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import './navBar.css';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Nav() {
-  const [opened, setOpened] = useState(false);
+	const [ opened, setOpened ] = useState(false);
 
-  return (
-    <nav className="navBar">
-      <div onClick={() => setOpened(!opened)}>
-        <FontAwesomeIcon icon={faBars} />
-      </div>
-      <h1>الموجه الاكاديمي</h1>
-      <div
-        className={opened ? "app_drawer app_drawer--visible " : "app_drawer"}
-      >
-        <div className="app_drawer__items">
-          <ul className="app_drawer__title">
-            <li>
-              <a href="/fit">ملائمة موضوع</a>
-            </li>
-            <li>
-              <a href="/">مواضيع</a>
-            </li>
-            <li>
-              <a href="/academic_institutes">مؤسسات أكاديمية</a>
-            </li>
-            <li>
-              <a href="/">مرشدين</a>
-            </li>
-            <li>
-              <a href="/">مدونة</a>
-            </li>
-            <li>
-              <a href="/">مقالات</a>
-            </li>
-            <li>
-              <a href="/">منح</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="signImage">
-        <img src="/lockB.png" alt="pro" />
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="navBar">
+			<div onClick={() => setOpened(!opened)}>
+				<FontAwesomeIcon icon={faBars} />
+			</div>
+			<h1>الموجه الاكاديمي 🎓</h1>
+			<div className={opened ? 'app_drawer app_drawer--visible ' : 'app_drawer'}>
+				<div className="app_drawer__items">
+					<ul className="app_drawer__title">
+						<li>
+							<a href="/fit">ملائمة موضوع</a>
+						</li>
+						<li>
+							<a href="/subjects">مواضيع</a>
+						</li>
+						<li>
+							<a href="/academic_institutes">مؤسسات أكاديمية</a>
+						</li>
+						<li>
+							<a href="/">مرشدين</a>
+						</li>
+						<li>
+							<a href="/">مدونة</a>
+						</li>
+						<li>
+							<a href="/">مقالات</a>
+						</li>
+						<li>
+							<a href="/">منح</a>
+						</li>
+						<li>
+							<a href="/">الصفحة الرئيسية</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="signImage">
+				<img src="/lockB.png" alt="pro" />
+			</div>
+		</nav>
+	);
 }
 
 export default Nav;
