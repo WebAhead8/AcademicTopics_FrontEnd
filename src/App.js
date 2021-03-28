2c931b007b0b6139ae4bbd646e14e026408d9c25
AlaaMath <alaa.lathqani@gmail.com>
2021-03-28 18:27:17 +0300

    fixing

--- src/App.js
+++ src/App.js
@@ -7,38 +7,14 @@ import {
 } from "react-router-dom";
 
 //Pages
-<<<<<<< HEAD
-import Home from './pages/Home';
-import Fit from './pages/fit';
-import LogIn from './pages/login';
-import SignUp from './pages/signup';
-import AcademicInstitutes from './pages/academic_institutes';
-import Subjects from './pages/subjects';
-
-function App() {
-	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" component={Home} />
-				<Route exact path="/fit" component={Fit} />
-				<Route exact path="/login" component={LogIn} />
-				<Route exact path="/signup" component={SignUp} />
-				<Route exact path="/academic_institutes" component={AcademicInstitutes} />
-				<Route exact path="/subjects" component={Subjects} />
-				<Redirect to="/404" />
-			</Switch>
-		</Router>
-	);
-=======
 import Home from "./pages/Home";
 import Fit from "./pages/fit";
 import LogIn from "./pages/login";
 import SignUp from "./pages/signup";
 import Blog from "./pages/blog";
-
-import AcademicInstitutes from "./pages/academic_institutes";
 import IdBlog from "./components/idblogs";
-// import Subjects from "./pages/subjects";
+import AcademicInstitutes from "./pages/academic_institutes";
+import Subjects from "./pages/subjects";
 
 function App() {
   return (
@@ -50,19 +26,12 @@ function App() {
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/blogs" component={Blog} />
         <Route exact path="/blog/:id" component={IdBlog} />
-        <Route
-          exact
-          path="/academic_institutes"
-          component={AcademicInstitutes}
-        />
-
-        {/* <Route exact path="/subject" component={subjects} /> */}
-        {/* <Route exact path="/subject" component={subjects} /> */}
+        <Route exact path="/academic_institutes" component={AcademicInstitutes} />
+        <Route exact path="/subjects" component={Subjects} />
         <Redirect to="/404" />
       </Switch>
     </Router>
   );
->>>>>>> d508f255f47d29024039c19f756ea1f8b475cf68
 }
 
 export default App;
